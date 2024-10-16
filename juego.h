fdd144da660d5e1c920b98bb08b018cbceb4d5b3
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2024-10-16 20:47:45 -0300

    Update juego.h

--- juego.h
+++ juego.h
@@ -1,7 +1,12 @@
 #pragma once
 
+using namespace std;
+
 void jugar(int &estadisticasGanador, int &estadisticasRondas);
 
 int tirarDado();
 void tiradaDeDados(int vDados[], int cantidad);
 void mostrarDados(int vDados[], int cantidad);
+int ejecutarRonda(int jugador, int bloqueador1, int bloqueador2);
+int calcularPuntaje(int vDados[], int cantidad, int bloqueador1, int bloqueador2);
+

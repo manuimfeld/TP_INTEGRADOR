7336fa25687505225e17a427c31c48323de69871
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2024-10-16 20:46:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 #include <ctime>
 #include "menu.h"
+#include "juego.h"
 
 using namespace std;
 
+
+
 int main()
 {
-    int opcion;
-    int estadisticasGanador = 0, estadisticasRondas = 0;
-    srand(time(0));
+     int estadisticasGanador=0, estadisticasRondas=0;
+     srand(time(0));
 
-    do
-    {
-        system("cls");
+menu(estadisticasGanador, estadisticasRondas);
 
-        opcion = seleccionarOpcion();
-        ejecutarOpcion(opcion, estadisticasGanador, estadisticasRondas);
-
-        cout << "Presione cualquier tecla para continuar ...";
-        system("pause>nul");
-    } while (opcion != 0);
-
-    return 0;
+return 0;
 }

40fb4b4420acd996b849373e72545470e9d8942d
manuimfeld <manu_jadir@hotmail.com>
2024-10-24 16:16:08 -0300

    separando responsabilidades

--- juego.cpp
+++ juego.cpp
@@ -2,103 +2,10 @@
 #include <limits>
 #include "dados.h"
 #include "juego.h"
+#include "ronda.h"
+#include "tirada.h"
 
 using namespace std;
-
-void mensaje()
-{
-    cout << "HOLA LOQUITOS";
-}
-
-string nombreJugador()
-{
-    string nombre;
-
-    cin >> nombre;
-
-    return nombre;
-}
-
-bool nuevaTirada()
-{
-    int opcion;
-    while (true) // Este bucle se va a finalizar cuando aparece un return
-    {
-
-        cout << endl;
-        cout << "---------------------" << endl;
-        cout << "Queres una tirada mas?" << endl;
-        cout << "0. No" << endl;
-        cout << "1. Si" << endl;
-        cin >> opcion;
-
-        // Acá verificamos si el usuario no ingresó un número, así el programa no se rompe o hace algo inesperado
-        if (cin.fail())
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignoramos la entrada incorrecta
-            cout << "Opcion incorrecta! Ingresa un numero valido." << endl;
-            continue; // Volvemos al inicio del bucle
-        }
-
-        // Si llegamos acá, es porque la opción fue un número (entrada válida)
-        switch (opcion)
-        {
-        case 0:
-            system("cls");
-            return false;
-        case 1:
-            return true;
-        default:
-            system("cls");
-            cout << "Opcion incorrecta!" << endl;
-            system("pause");
-            // Acá no usamos continue, ya que el programa va a volver a pedir la opcion de nuevo
-            break;
-        }
-    }
-}
-
-bool nuevaRonda()
-{
-    int opcion;
-    while (true) // Este bucle se va a finalizar cuando aparece un return
-    {
-        cout << endl;
-        cout << "Queres jugar una ronda mas? " << endl;
-        cout << "0. No" << endl;
-        cout << "1. Si" << endl;
-        cin >> opcion;
-
-        // Acá verificamos si el usuario no ingresó un número, así el programa no se rompe o hace algo inesperado
-        if (cin.fail())
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignoramos la entrada incorrecta
-            cout << "Opcion incorrecta! Ingresa un numero valido." << endl;
-            continue; // Volvemos al inicio del bucle
-        }
-
-        // Si llegamos acá, es porque la opción fue un número (entrada válida)
-        switch (opcion)
-        {
-        case 0:
-            system("cls");
-            cout << "Fin del juego." << endl;
-            cout << endl;
-            return false;
-        case 1:
-            system("cls");
-            return true;
-        default:
-            system("cls");
-            cout << "Opcion incorrecta!" << endl;
-            system("pause");
-            // Acá no usamos continue, ya que el programa va a volver a pedir la opcion de nuevo
-            break;
-        }
-    }
-}
 
 int calcularPuntaje(int vDados[5], int dadosDisponibles, int bloqueador1, int bloqueador2)
 {
@@ -211,7 +118,7 @@ void jugarModoSolitario(string nombreJugador)
             cout << "Puntaje total de la partida hasta ahora: " << puntajeTotal << endl;
             dadosDisponibles = 5;
             continuarRonda = nuevaRonda();
-                }
+        }
 
         // Sumamos el puntaje de la ronda en el puntaje total de la partida
         puntajeTotal += puntajeTotalRonda;
---
+++ tirada.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+bool nuevaTirada()
+{
+    int opcion;
+    while (true) // Este bucle se va a finalizar cuando aparece un return
+    {
+
+        cout << endl;
+        cout << "---------------------" << endl;
+        cout << "Queres una tirada mas?" << endl;
+        cout << "0. No" << endl;
+        cout << "1. Si" << endl;
+        cin >> opcion;
+
+        // Acá verificamos si el usuario no ingresó un número, así el programa no se rompe o hace algo inesperado
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignoramos la entrada incorrecta
+            cout << "Opcion incorrecta! Ingresa un numero valido." << endl;
+            continue; // Volvemos al inicio del bucle
+        }
+
+        // Si llegamos acá, es porque la opción fue un número (entrada válida)
+        switch (opcion)
+        {
+        case 0:
+            system("cls");
+            return false;
+        case 1:
+            return true;
+        default:
+            system("cls");
+            cout << "Opcion incorrecta!" << endl;
+            system("pause");
+            // Acá no usamos continue, ya que el programa va a volver a pedir la opcion de nuevo
+            break;
+        }
+    }
+}

04a6535dba6f4286827abc4b783318fdc2e1b72b
manuimfeld <manu_jadir@hotmail.com>
2024-10-23 21:34:27 -0300

    nuevo commit

--- creditos.cpp
+++ creditos.cpp
@@ -7,7 +7,7 @@ void mostrarCreditos()
 {
     cout << "Creditos: Equipo X" << endl;
     cout << "Integrantes:" << endl;
-    cout << "Fiaschi Bernardo - Legajo X" << endl;
+    cout << "Fiaschi Bernardo - Escribi tu Legajo X" << endl;
     cout << "Peña Pariona Cinthia - Legajo X" << endl;
     cout << "Serafini Nicolas - Legajo X" << endl;
     cout << "Imfeld Jair Manuel - Legajo 30397" << endl;

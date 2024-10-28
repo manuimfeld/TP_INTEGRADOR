aa5e9409564bce45d89fa2d9261531d14f534a48
manuimfeld <manu_jadir@hotmail.com>
2024-10-28 02:44:13 -0300

    cambio de nombre a las variables y mejor funcionamiento de rondas

--- turnos.cpp
+++ turnos.cpp
@@ -2,25 +2,35 @@
 #include "tirada.h"
 #include "dados.h"
 
-int ejecutarTurnoJugador(string nombreJugador, int dados[], int &dadosDisponibles, int bloqueador1, int bloqueador2)
+int ejecutarTurnoJugador(string nombreJugador, int dados[], int &dadosDisponibles, int bloqueador1, int bloqueador2, int &puntajeTotal)
 {
-    int puntajeTurno = 0;
+    int puntajeRonda = 0; // Acá vamos a ir guardando el puntaje de la ronda
+    int tiradaActual = 0;
     bool continuarTirada = true;
 
+    // Cada vez que empiece una tirada o el jugador tenga dados disponibles, se ejecutan las siguientes funciones del bucle
     while (continuarTirada && dadosDisponibles > 0)
     {
-        cout << "Turno de " << nombreJugador << endl;
-        int puntajeTirada = ejecutarTirada(dados, dadosDisponibles, bloqueador1, bloqueador2);
-        puntajeTurno += puntajeTirada;
+        tiradaActual++;
+        int puntajeTirada = ejecutarTirada(dados, dadosDisponibles, bloqueador1, bloqueador2, tiradaActual, puntajeRonda); // Guardamos el puntaje de la tirada
 
-        dadosDisponibles -= calcularDadosDisponibles(dados, dadosDisponibles, bloqueador1, bloqueador2);
-        continuarTirada = (dadosDisponibles > 0) ? nuevaTirada() : false;
+        puntajeRonda += puntajeTirada;                                                                   // Guardamos el puntaje del jugador
+        dadosDisponibles -= calcularDadosDisponibles(dados, dadosDisponibles, bloqueador1, bloqueador2); // Si hubo algun dado igual a un bloqueador, restamos los dados disponibles
 
         if (dadosDisponibles == 0)
         {
-            cout << "Sin dados disponibles. Puntaje de turno reiniciado." << endl;
-            puntajeTurno = 0; // Puntaje a cero si se queda sin dados
+            cout << "Te quedaste sin dados disponibles, el puntaje de la ronda vuelve a 0" << endl;
+            puntajeRonda = 0; // Puntaje a cero si se queda sin dados
+            continuarTirada = false;
+        }
+        else
+        {
+            puntajeTotal += puntajeTirada; // Sumamos este puntaje al TOTAL de la partida (el puntaje que obtuvo a lo largo de las rondas)
+            cout << "---------------------" << endl;
+            cout << "Puntaje de la ronda: " << puntajeRonda << endl;
+            cout << "Puntaje total: " << puntajeTotal << endl;
+            continuarTirada = nuevaTirada(); // Le preguntamos al jugador si quiere una tirada más
         }
     }
-    return puntajeTurno;
+    return puntajeRonda;
 }

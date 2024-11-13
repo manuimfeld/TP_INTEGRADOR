7ef5a240573798b649ee1f811e23f4d4e56bfc8e
BernardoFiaschi <bernardo.fiaschi@gmail.com>
2024-11-13 20:38:30 -0300

    reglas

--- reglas.cpp
+++ reglas.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 void reglas()
 {
-    cout << "Las reglas del juego son las siguientes: " << endl;
+    cout << "Las reglas del juego son las siguientes: " << endl << endl;
     cout << "El juego consiste en una serie de tres rondas, en las cuales los jugadores deberán tirar dos dados llamados bloqueadores y ";
     cout << "numerosas tiradas de hasta cinco dados." << endl << endl;
     cout << "Por cada tirada de una ronda se deberan sumar todos los dados de la tirada cuyos valores no coincidan con los dados bloqueadores." << endl << endl;
-    cout << "Cada dado cuyo valor sea igual al de un bloqueador, no podrá utilizarse en las siguientes tiradas de la ronda.";
+    cout << "Cada dado cuyo valor sea igual al de un bloqueador, no podra utilizarse en las siguientes tiradas de la ronda.";
     cout << "Disminuyendo asi la cantidad de dados a tirar." << endl << endl;
     cout << "Al finalizar cada tirada se preguntara si desea continuar tirando o no" << endl << endl;
     cout << "Si un jugador obtiene todos los dados iguales y estos no son iguales a los bloqueadores";
